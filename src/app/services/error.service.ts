43e44ef76a96994231f93ad31fb793f090ace747
Leonardo Lira C <leon_lira_@hotmail.com>
2021-12-18 11:39:03 -0600

    cambio de liena

--- src/app/services/error.service.ts
+++ src/app/services/error.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { NotificationService } from './notification.service';
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
 
 export class ErrorService {
-    constructor(private notificationService: NotificationService){}
-    logSomeMessage(msg: any, objecterror: any, status: number) {
-       
+    constructor(private notificationService: NotificationService) {}
+    public logSomeMessage(msg: any, objecterror: any, status: number) {
+
     }
 }
